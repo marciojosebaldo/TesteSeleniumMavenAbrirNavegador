f9548ef22af48cec9a650c62aa636039f134a111
marciojosebaldo <marciojosebaldo@gmail.com>
2023-08-22 21:36:50 -0300

    Add files via upload

--- src/main/java/Testes/TesteNavegador.java
+++ src/main/java/Testes/TesteNavegador.java
@@ -2,24 +2,22 @@ package Testes;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class TesteNavegador {
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeMethod
     public void iniciar() {
         System.setProperty("webdriver.edge.driver", "C:\\Users\\Aluno\\Desktop\\TesteSelenium\\Driver\\edge\\msedgedriver.exe");
         driver = new EdgeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     // Encerrar o teste
@@ -31,7 +29,13 @@ public class TesteNavegador {
     // Abrir o navegador e abrir o site
     @Test
     public void AbrirNavegadorAbrirGoogle() {
-        driver.get("https://www.google.com.br/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://grupoassociativogasp.com.br/gestor_site/login");
+
+        driver.findElement(By.id("cd_usuario")).sendKeys("Marcio");
+        driver.findElement(By.id("ds_senha")).sendKeys("senha");
+
+        WebElement botaoLogin = driver.findElement(By.xpath("/html/body/div[2]/form[1]/div[4]/label/button"));
+        botaoLogin.click();
     }
 }
